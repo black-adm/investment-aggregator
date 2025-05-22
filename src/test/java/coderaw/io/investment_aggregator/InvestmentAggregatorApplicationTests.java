b4ef9b3aaa18c5f4333585b73443991d6eb8b2ef
black-adm <mattheuscontato17@gmail.com>
2025-05-22 19:09:53 -0300

    project base

---
+++ src/test/java/coderaw/io/investment_aggregator/InvestmentAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package coderaw.io.investment_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
