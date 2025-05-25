f38d29e810802ee5e29049a8e7acb3218d74b671
black-adm <mattheuscontato17@gmail.com>
2025-05-25 17:54:00 -0300

    fix: added argument capture to user creation service tests

--- src/test/java/coderaw/io/investment_aggregator/service/UserServiceTest.java
+++ src/test/java/coderaw/io/investment_aggregator/service/UserServiceTest.java
@@ -7,16 +7,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -26,20 +27,52 @@ class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
+    @Captor
+    private ArgumentCaptor<User> userArgumentCaptor;
+
     @Nested
     class createUser {
         @Test
         @DisplayName("Should create a user with success")
-        void shoudlCreateAUserWithSuccess() {
-            var user = new User("john.doe", "johndoe@example.com", "admin1234");
+        void shouldCreateAUserWithSuccess() {
+            var user = new User(
+                    "john.doe",
+                    "johndoe@example.com",
+                    "admin1234"
+            );
+
             user.setUserId(UUID.randomUUID());
+            doReturn(user).when(userRepository).save(userArgumentCaptor.capture());
 
-            doReturn(user).when(userRepository).save(any());
+            var input = new UserRequestDto(
+                    "john.doe",
+                    "johndoe@example.com",
+                    "admin1234"
+            );
 
-            var input = new UserRequestDto("john.doe", "johndoe@example.com", "admin1234");
             UUID output = userService.createUser(input);
-
             assertNotNull(output);
+
+            var capturedValue = userArgumentCaptor.getValue();
+            assertEquals(input.username(), capturedValue.getUsername());
+            assertEquals(input.email(), capturedValue.getEmail());
+            assertEquals(input.password(), capturedValue.getPassword());
+        }
+
+        @Test
+        @DisplayName("Should throw exception when error occurs")
+        void shouldThrowExceptionWhenErrorOccurs() {
+            doThrow(new RuntimeException())
+                    .when(userRepository)
+                    .save(userArgumentCaptor.capture());
+
+            var input = new UserRequestDto(
+                    "john.doe",
+                    "johndoe@example.com",
+                    "admin1234"
+            );
+
+            assertThrows(RuntimeException.class, () -> userService.createUser(input));
         }
     }
 }
